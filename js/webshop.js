980653f2eb663ed455a3db9cb5b0ee34c4f4a2fa
FutureSaiyann <a.berk@miniemen.school>
2026-06-11 11:30:34 +0200

    latest

--- js/webshop.js
+++ js/webshop.js
@@ -13,20 +13,20 @@
 async function renderHomeProducten() {
     const grid = document.querySelector("#product-grid");
     if (!grid) return;
-    let producten = await runQuery("SELECT Afbeelding, Naam as naam FROM producten");
+   let producten = await runQuery("SELECT Afbeelding, Naam, Prijs, description FROM producten");
     grid.innerHTML = "";
     producten.forEach(product => {
-        const html = `
-            <div class="product-card">
-                <img src="${product.Afbeelding}" alt="${product.Afbeelding}">
-                <h3>${product.naam}</h3>
-                <p class="price">€${product.prijs}</p>
-                <p class="description">${product.description}</p>
-                <button class="add-to-cart">Add to Cart</button>
-            </div>
-        `;
-        grid.innerHTML += html;
-    });
+    const html = `
+        <div class="product-card">
+            <img src="${product.Afbeelding}" alt="${product.Naam}">
+            <h3>${product.Naam}</h3>
+            <p class="price">€${product.Prijs}</p>
+            <p class="description">${product.description ?? ''}</p>
+            <button class="add-to-cart">Add to Cart</button>
+        </div>
+    `;
+    grid.innerHTML += html;
+});
 }
 
 // document.addEventListener("DOMContentLoaded", () => {
